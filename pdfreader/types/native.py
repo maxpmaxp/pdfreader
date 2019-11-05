aabde28a96abb64fd029f0993b4841fd1493d1e9
Maksym Polshcha <maxp@sterch.net>
2019-11-05 13:17:27 -0500

    RunLengthDecode fixed

--- pdfreader/types/native.py
+++ pdfreader/types/native.py
@@ -157,7 +157,6 @@ class Stream(object):
         res = b''
         buffer = []
         state = 'need_length'
-
         for c in data:
             if state == 'need_length':
                 if c == 128:
@@ -170,11 +169,11 @@ class Stream(object):
                 else:
                     state = 'need_many'
             elif state == 'need_one':
-                res += bytes([c] * (length + 1))
+                res += bytes([c] * (257 - length))
                 state = 'need_length'
             elif state == 'need_many':
                 buffer.append(c)
-                if len(buffer) == 257 - length:
+                if len(buffer) == length + 1:
                     res += bytes(buffer)
                     buffer = []
                     state = 'need_length'
@@ -184,8 +183,6 @@ class Stream(object):
             res = b''
 
         return res
-
-
 
     def filter_ASCIIHexDecode(self, data):
         """
