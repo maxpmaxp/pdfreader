919c02bea94be0c727b31952ecda4c3e193bd69c
Max Polshcha <max.polshcha@upstart.com>
2024-05-03 13:20:42 -0400

    DCT encoding support

--- pdfreader/filters/dct.py
+++ pdfreader/filters/dct.py
@@ -1,5 +1,8 @@
+from PIL import Image
+
 filter_names = ('DCTDecode', 'DCT')
 
 
-def decode(binary, params):
-    raise NotImplementedError('DCTDecode')
+def decode(binary, *_):
+    img = Image.frombytes(binary)
+    return img.tobytes()
